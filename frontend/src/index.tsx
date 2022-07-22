84c2a3e01f843433e3027b9fc037297e571722c8
hokke-jp <burihokke@gmail.com>
2022-07-22 17:31:10 +0900

    #10 ローカルにNginx+Rails+MySQLの本番環境構築

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
+import './index.css'
+import './tailwind.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './component/App'
