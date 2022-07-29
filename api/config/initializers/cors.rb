647d6f57265f82ab12bcbde0e711b58be7d627a7
hokke-jp <burihokke@gmail.com>
2022-07-29 11:50:53 +0900

    #11 S3に静的ファイルを置き,ECSにバックエンドを配置するAWS環境構築

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost", "https://www.rekishichizu.jp"
+    origins "http://localhost", "https://www.rekishichizu.jp"
 
     resource "*",
       headers: :any,
