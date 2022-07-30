d2b64fe56f20ab2c70bc4b2670934024bbc6ed86
hokke-jp <burihokke@gmail.com>
2022-07-30 13:46:25 +0900

    ESLint,Prettier,Huskyの調整

---
+++ frontend/src/posts.test.js
@@ -0,0 +1,23 @@
+import { unmountComponentAtNode } from 'react-dom'
+// import { createRoot } from 'react-dom/client';
+// import { act } from "react-dom/test-utils";
+
+// import { Posts } from "./component/posts";
+
+let container = null
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
+it('投稿が表示される', () => {
+  expect(1 + 2).toBe(3)
+})
