d95d2f646d83c61d7603ed29f337cfdcf3962b5f
hokke-jp <burihokke@gmail.com>
2022-08-02 15:09:34 +0900

    #13 CircleCIでECR,ECSへデプロイテスト05

--- api/app/controllers/posts_controller.rb
+++ api/app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  def root
+    render json: {'greeting' => 'Hello!', 'time' => Time.zone.now}
+  end
+
   def index
     posts = Post.all
     render json: posts
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'posts#root'
   get '/posts', to: 'posts#index'
 end
