c16d0a8a382a46cb01efe94631fdeda5eaf2fb5e
hokke-jp <burihokke@gmail.com>
2022-09-02 12:26:46 +0900

    #37 パスワード表示切り替え機能実装

--- frontend/src/components/user/CreateAccount.tsx
+++ frontend/src/components/user/CreateAccount.tsx
@@ -1,6 +1,14 @@
 import { axiosInstance } from '../../utils/axios'
 import { CurrentUserContext } from './CurrentUserContext'
+import { VisibilityOff, Visibility } from '@mui/icons-material'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import {
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton
+} from '@mui/material'
 import Alert from '@mui/material/Alert'
 import Avatar from '@mui/material/Avatar'
 import Box from '@mui/material/Box'
@@ -17,12 +25,14 @@ import { NavLink, useNavigate } from 'react-router-dom'
 
 const theme = createTheme()
 export const CreateAccount = () => {
+  // 入力情報送信機能
   const navigate = useNavigate()
   const [errorMessage, setErrorMessage] = useState('')
   const { setCurrentUser } = useContext(CurrentUserContext)
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const data = new FormData(event.currentTarget)
+    console.log(data)
     ;(async () => {
       setErrorMessage('')
       return await axiosInstance
@@ -46,6 +56,37 @@ export const CreateAccount = () => {
           setErrorMessage(error.response.data.errors.full_messages.join('\n'))
         })
     })()
+  }
+
+  // パスワード表示切り替え機能
+  interface State {
+    amount: string
+    password: string
+    weight: string
+    weightRange: string
+    showPassword: boolean
+  }
+  const [values, setValues] = useState<State>({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false
+  })
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value })
+    }
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword
+    })
+  }
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault()
   }
 
   return (
@@ -106,15 +147,38 @@ export const CreateAccount = () => {
                 />
               </Grid>
               <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="パスワード"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
+                <FormControl sx={{ width: '100%' }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    パスワード *
+                  </InputLabel>
+                  <OutlinedInput
+                    required
+                    fullWidth
+                    name="password"
+                    type={values.showPassword ? 'text' : 'password'}
+                    id="outlined-adornment-password"
+                    autoComplete="new-password"
+                    value={values.password}
+                    onChange={handleChange('password')}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {values.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="パスワード"
+                  />
+                </FormControl>
               </Grid>
             </Grid>
             <Button
