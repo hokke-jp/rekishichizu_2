d76a0c98548c6886497e2e6e6a2937cef75f43e1
hokke-jp <burihokke@gmail.com>
2022-10-12 21:24:15 +0900

    #55 Gem Ransackを導入し,検索機能実装

--- frontend/src/Views/SearchDrawer.tsx
+++ frontend/src/Views/SearchDrawer.tsx
@@ -10,13 +10,13 @@ import {
   MenuItem,
   OutlinedInput,
   Select,
-  SelectChangeEvent,
-  Theme,
-  useTheme
+  SelectChangeEvent
 } from '@mui/material'
 import { PERIODS } from 'Constant/PERIOD'
 import { PREFECTURES } from 'Constant/PREFECTURE'
-import { useState } from 'react'
+import { useArticlesContext } from 'Utils/ArticlesContext'
+import { axiosInstance } from 'Utils/axios'
+import { ChangeEvent, useState } from 'react'
 
 const ITEM_HEIGHT = 48
 const ITEM_PADDING_TOP = 8
@@ -29,29 +29,60 @@ const MenuProps = {
   }
 }
 
-function getStyles(name: string, prefectures: readonly string[], theme: Theme) {
-  return {
-    fontWeight:
-      prefectures.indexOf(name) === -1 ? theme.typography.fontWeightRegular : theme.typography.fontWeightMedium
-  }
+const sortByAsc = (ary: number[]): number[] => {
+  return ary.sort((a, b) => {
+    if (a < b) return -1
+    if (a > b) return 1
+    return 0
+  })
 }
 
 export const SearchDrawer = () => {
-  const theme = useTheme()
-  const [periods, setPeriods] = useState<string[]>([])
-  const [prefectures, setPrefecurets] = useState<string[]>([])
+  const [words, setWords] = useState<string>('')
+  const [periods, setPeriods] = useState<number[]>([])
+  const [prefectures, setPrefecurets] = useState<number[]>([])
+  const { setArticles } = useArticlesContext()
 
-  const handlePeriod = (event: SelectChangeEvent<typeof prefectures>) => {
-    const {
-      target: { value }
-    } = event
-    setPeriods(typeof value === 'string' ? value.split(',') : value)
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setWords(event.target.value)
   }
-  const handlePrefecture = (event: SelectChangeEvent<typeof prefectures>) => {
+
+  const handlePeriod = (event: SelectChangeEvent<string | string[]>) => {
     const {
       target: { value }
     } = event
-    setPrefecurets(typeof value === 'string' ? value.split(',') : value)
+    setPeriods(
+      typeof value === 'string' ? [PERIODS.indexOf(value) + 1] : sortByAsc(value.map((val) => PERIODS.indexOf(val) + 1))
+    )
+  }
+  const handlePrefecture = (event: SelectChangeEvent<string | string[]>) => {
+    const {
+      target: { value }
+    } = event
+    setPrefecurets(
+      typeof value === 'string'
+        ? [PREFECTURES.indexOf(value) + 1]
+        : sortByAsc(value.map((val) => PREFECTURES.indexOf(val) + 1))
+    )
+  }
+
+  const handleSearch = () => {
+    const removeSpace = (str: string): string => {
+      return str.replace(/^\s+|\s+$/g, '').replace(/\s+/g, ',')
+    }
+    const removedWords = removeSpace(words)
+    axiosInstance
+      .get('/articles', {
+        params: {
+          words: removedWords,
+          period_ids: `${periods}`,
+          prefecture_ids: `${prefectures}`
+        }
+      })
+      .then((response) => {
+        console.log('response : ', response)
+        setArticles(response.data)
+      })
   }
 
   return (
@@ -61,8 +92,8 @@ export const SearchDrawer = () => {
         id="search-drawer-div"
         className="flex h-screen rounded-tr-3xl rounded-br-3xl bg-white shadow-lg overflow-hidden"
       >
-        <div className="grow flex flex-col gap-y-10 pt-10 pr-2 pl-8">
-          <div className="grid grid-rows-2 grid-cols-2 h-24 border border-[#B7B7B7] rounded-2xl overflow-hidden">
+        <div className="grow flex flex-col gap-y-10 min-h-screen py-10 pr-2 pl-8 overflow-scroll">
+          <div className="grid grid-rows-2 grid-cols-2 min-h-[96px] border border-[#B7B7B7] rounded-2xl overflow-hidden">
             <ButtonBase>
               <input type="radio" id="latest" name="drone" value="latest" className="peer" hidden defaultChecked />
               <label
@@ -82,18 +113,18 @@ export const SearchDrawer = () => {
               </label>
             </ButtonBase>
             <ButtonBase>
-              <input type="radio" id="old" name="drone" value="old" className="peer" hidden />
+              <input type="radio" id="asc" name="drone" value="asc" className="peer" hidden />
               <label
-                htmlFor="old"
+                htmlFor="asc"
                 className="flex items-center justify-center w-full h-full peer-checked:bg-[#1876D3] peer-checked:text-white peer-checked:border-0 border-[#B7B7B7] border-r"
               >
                 <p>古い</p>
               </label>
             </ButtonBase>
             <ButtonBase>
-              <input type="radio" id="new" name="drone" value="new" className="peer" hidden />
+              <input type="radio" id="desc" name="drone" value="desc" className="peer" hidden />
               <label
-                htmlFor="new"
+                htmlFor="desc"
                 className="flex items-center justify-center w-full h-full peer-checked:bg-[#1876D3] peer-checked:text-white peer-checked:border-0 border-[#B7B7B7]"
               >
                 <p>新しい</p>
@@ -106,14 +137,17 @@ export const SearchDrawer = () => {
             <OutlinedInput
               id="outlined-adornment-search"
               type="text"
-              // value={values.search}
-              // onChange={handleChange('search')}
+              value={words}
+              onChange={handleChange}
               sx={{ borderRadius: '30px' }}
               endAdornment={
-                <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={handleSearch}>
                   <SearchIcon />
                 </IconButton>
               }
+              onKeyDown={(e) => {
+                if (e.keyCode === 13) handleSearch()
+              }}
               label="キーワード検索"
             />
           </FormControl>
@@ -125,7 +159,7 @@ export const SearchDrawer = () => {
               id="demo-multiple-chip"
               multiple
               fullWidth
-              value={periods}
+              value={periods.map((period) => PERIODS[period - 1])}
               onChange={handlePeriod}
               input={<OutlinedInput id="select-multiple-chip" label="時代" />}
               renderValue={(selected) => (
@@ -138,7 +172,7 @@ export const SearchDrawer = () => {
               MenuProps={MenuProps}
             >
               {PERIODS.map((period) => (
-                <MenuItem key={period} value={period} style={getStyles(period, periods, theme)}>
+                <MenuItem key={period} value={period}>
                   {period}
                 </MenuItem>
               ))}
@@ -152,7 +186,7 @@ export const SearchDrawer = () => {
               id="demo-multiple-chip"
               multiple
               fullWidth
-              value={prefectures}
+              value={prefectures.map((prefecture) => PREFECTURES[prefecture - 1])}
               onChange={handlePrefecture}
               input={<OutlinedInput id="select-multiple-chip" label="都道府県" />}
               renderValue={(selected) => (
@@ -165,14 +199,14 @@ export const SearchDrawer = () => {
               MenuProps={MenuProps}
             >
               {PREFECTURES.map((prefecture) => (
-                <MenuItem key={prefecture} value={prefecture} style={getStyles(prefecture, prefectures, theme)}>
+                <MenuItem key={prefecture} value={prefecture}>
                   {prefecture}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
 
-          <Button variant="contained" sx={{ width: '100px', mx: 'auto', mt: 3 }}>
+          <Button variant="contained" sx={{ width: '100px', mx: 'auto', mt: 3 }} onClick={handleSearch}>
             検索
           </Button>
         </div>
