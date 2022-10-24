05dc76b21731daafe3363801dce2886916480a08
hokke-jp <burihokke@gmail.com>
2022-10-24 11:46:20 +0900

    [hotfix] Railsコンテナが正常に動作しない症状への対応3

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -32,8 +32,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
-  Rails.application.routes.default_url_options[:host] = 'localhost'
-  Rails.application.routes.default_url_options[:port] = 3000
+  Rails.application.routes.default_url_options[:host] = 'api.rekishichizu.jp'
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
