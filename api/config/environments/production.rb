959c9302b6b460dedd724268327a6db8d01d8584
hokke-jp <burihokke@gmail.com>
2022-10-24 09:51:08 +0900

    [hotfix] ECSコンテナが起動しない症状の対応として,api-Railsコンテナ起動後に実行するコマンドを変更5

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
+  Rails.application.routes.default_url_options[:host] = 'localhost'
+  Rails.application.routes.default_url_options[:port] = 3000
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
